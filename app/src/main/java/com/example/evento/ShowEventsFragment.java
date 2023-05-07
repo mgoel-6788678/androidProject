0c09a782593ebff26b063ef25a08d99f301f7cc3
Mehul Goel <mehul@Mehuls-MacBook-Air.local>
2023-05-07 22:04:06 +0530

    few linkages so everything is visible

--- app/src/main/java/com/example/evento/ShowEventsFragment.java
+++ app/src/main/java/com/example/evento/ShowEventsFragment.java
@@ -1,5 +1,6 @@
 package com.example.evento;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.FirebaseDatabase;
@@ -18,11 +20,23 @@ public class ShowEventsFragment extends Fragment {
     RecyclerView recyclerView;
     ShowEventsAdapter showEventsAdapter;
 
+    Button donateButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_show_events, container, false);
+
+        donateButton = view.findViewById(R.id.donate);
+
+        donateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(), PayWall.class);
+                startActivity(i);
+            }
+        });
 
         recyclerView = view.findViewById(R.id.show_events_recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
